bef594d50af92d96a47af52e01b20812f2937278
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-04-30 00:33:16 +0900

    Used: C:\WORKS\WORKSPACES_ANDROID\HomeBase2

--- MyLib.java
+++ MyLib.java
@@ -4,7 +4,7 @@ import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import myapp.timerapp.R;
+//import myapp.timerapp.R;
 
 //import myapp.image.gallery.ImageFromGalleryActivity;
 
